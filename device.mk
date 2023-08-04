ea89751729f68f321858aace300ccb2be0111ebb
Ella Alinda <admin@ilolicon.com>
2023-08-04 12:16:38 +0800

    更新 device.mk

--- device.mk
+++ device.mk
@@ -131,14 +131,10 @@ PRODUCT_PACKAGES += \
 
 # IMS
 PRODUCT_BOOT_JARS += \
-    mediatek-carrier-config-manager \
     mediatek-common \
     mediatek-framework \
-    mediatek-gwsdv2 \
     mediatek-ims-base \
     mediatek-ims-common \
-    mediatek-ims-extension-plugin \
-    mediatek-services \
     mediatek-telecom-common \
     mediatek-telephony-base \
     mediatek-telephony-common \
